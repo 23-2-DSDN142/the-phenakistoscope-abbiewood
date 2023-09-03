f1b39cd61adf9544c1743b52a0f45cb784998571
abbiewood <139762394+abbiewood@users.noreply.github.com>
2023-09-03 16:55:02 +1200

    First trials

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -3,47 +3,52 @@ const SLICE_COUNT = 10;
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image("monstera" , "png");
+  pScope.load_image("zebrina" , "png");
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 245, 198, 218);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
 
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+  var layer1 = new PLayer(pot);
+  layer1.mode(RING);
+  layer1.set_boundary(0, 400);
+
+  var layer2 = new PLayer(monstera);
+  layer2.mode(SWIRL (2));
+  layer2.set_boundary(200, 700);
+
+  var layer3 = new PLayer(zebrina);
+  layer3.mode (SWIRL (3))
+  layer3.set_boundary (200, 700);
+
 }
 
-function faces(x, y, animation, pScope){
+function monstera(x, y, animation, pScope){
+  scale (0.5);
+  pScope.draw_image("monstera",x,y);
   
-  scale(animation.frame*2);
-
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
-
 }
 
-function squares(x, y, animation, pScope){
+function pot(x, y, animation, pScope){
 
   // this is how you set up a background for a specific layer
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  fill("#853225")
+  arc(x,y,600,600,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+}
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+function zebrina(x, y, animation, pScope){
+
+  scale (1)
+  pScope.draw_image("zebrina",x,y);
 
 }
