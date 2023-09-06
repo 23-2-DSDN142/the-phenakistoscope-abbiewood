2382e01dfd5042b8c624d90ef08be2b5ee2fdb2b
abbiewood <139762394+abbiewood@users.noreply.github.com>
2023-09-07 11:00:08 +1200

    Work on watering can

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -6,26 +6,59 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  
   pScope.load_image("monstera" , "png");
   pScope.load_image("zebrina" , "png");
+  pScope.load_image_sequence("watering", "png", 10)
+  pScope.load_image("monsteraCentre" , "png");
+
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 245, 198, 218);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, "#ffdee2");  //lets us draw the whole circle background, ignoring the boundaries
 
+  let plantPot = new PLayer(pot);
+  plantPot.mode(RING);
+  plantPot.set_boundary(0, 400);
 
-  var layer1 = new PLayer(pot);
-  layer1.mode(RING);
-  layer1.set_boundary(0, 400);
+  let monsteraPlant = new PLayer(monstera);
+  monsteraPlant.mode(SWIRL (1));
+  monsteraPlant.set_boundary(200, 700);
 
-  var layer2 = new PLayer(monstera);
-  layer2.mode(SWIRL (2));
-  layer2.set_boundary(200, 700);
+  let zebrinaPlant = new PLayer(zebrina);
+  zebrinaPlant.mode (SWIRL (1))
+  zebrinaPlant.set_boundary (200, 700);
 
-  var layer3 = new PLayer(zebrina);
-  layer3.mode (SWIRL (3))
-  layer3.set_boundary (200, 700);
+  let can = new PLayer (watering)
+  can.mode (RING)
+  can.set_boundary (1000, 1000)
+
+  // let layer5 = new PLayer (water)
+  // layer5.mode (SWIRL(1))
+  // layer5.set_boundary (900,1000)
+
+  let outerRing = new PLayer (outsideRing)
+  outerRing.mode (RING)
+  outerRing.set_boundary (980,1000)
+
+  // let outerPot = new PLayer (outerPotRing)
+  // outerPot.mode
+  // outerPot.set_boundary (500,300)
+
+  let monsteraMiddle = new PLayer (monsteraCentre)
+  monsteraMiddle.mode (RING)
+  monsteraMiddle.set_boundary (0,500)
+
+}
+
+function outerPotRing (x,y,animation,pScope){
+scale(0.5)
+pScope.fill_background("#3b2219")
+
+}
+function outsideRing (x,y,animation,pScope){
+  pScope.fill_background("#820e3f")
 
 }
 
@@ -50,5 +83,25 @@ function zebrina(x, y, animation, pScope){
 
   scale (1)
   pScope.draw_image("zebrina",x,y);
+
+}
+
+function watering(x,y, animation, pScope){
+scale (0.8);
+pScope.draw_image_from_sequence("watering",0, -980, animation.frame);
+
+}
+
+function water (x,y,animation, pScope){
+  
+  fill (255)
+  circle (0, 300, 80);
+
+}
+
+function monsteraCentre (x,y, animation, pScope) {
+scale (0.5);
+pScope.draw_image("monsteraCentre",0,0);
+
 
 }
